708f9952c94524ab64cf655079f175b6dc0e4980
Dhiyaa Nazim <dhysdrn@gmail.com>
2024-01-17 11:20:27 -0500

    Controller created

--- index.php
+++ index.php
@@ -1,2 +1,28 @@
 <?php
-echo "My Diner";
+/* Dhiyaa Nazim
+ * January 2024
+ * https://dhiyaa-naz.greenriverdev.com/328/diner/
+ * This is my CONTROLLER for the Diner app
+ */
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require the autoload file
+require_once ('vendor/autoload.php');
+
+// Instantiate Fat-Free framework (F3)
+$f3 = Base::instance(); // static method
+
+// Define a default route
+$f3->route('GET /', function () {
+    echo "My Diner";
+});
+
+// Run Fat-Free
+$f3->run(); // instance method
+
+
+
+
