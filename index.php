f56995ec38a062a58789ab42205ac04c84b28d11
Dhiyaa Nazim <dhysdrn@gmail.com>
2024-02-01 10:08:51 -0500

    Updated index.php and .gitignore

--- index.php
+++ index.php
@@ -17,7 +17,11 @@ $f3 = Base::instance(); // static method
 
 // Define a default route
 $f3->route('GET /', function () {
-    echo "My Diner";
+    //echo "My Diner";
+
+    // Display a view page
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 // Run Fat-Free
