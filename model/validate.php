87dd711411afc5d341550b500fd37288d2d18372
Dhiyaa Nazim <dhysdrn@gmail.com>
2024-02-15 12:59:22 -0500

    Validate food and meal

---
+++ model/validate.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * 328/model/validate.php
+ * Validate data for the diner app
+ */
+
+// Return true if food is valid
+function validFood($food){
+    if(trim($food) == "")
+        return false;
+    if(!ctype_alpha($food))
+        return false;
+    return true;
+}
+
+function validMeal($meal)
+{
+    return in_array($meal, getMeals());
+}
